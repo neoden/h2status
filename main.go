7e3820293b9203c5b53418d5b69f56b2c36513c8
neoden <lenar.imamutdinov@gmail.com>
2022-09-25 10:04:17 +0300

    battery status modes

--- main.go
+++ main.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"bufio"
 	"encoding/binary"
 	"encoding/json"
 	"fmt"
 	"io"
 	"io/ioutil"
+	"log"
 	"os"
 	"strconv"
 	"strings"
@@ -13,6 +15,68 @@ import (
 
 	"golang.org/x/sys/unix"
 )
+
+var l = log.New(os.Stderr, "", 0)
+
+const BATTERY_STATE_MODE_PECENTAGE = 0
+const BATTERY_STATE_MODE_REMAINING_TIME = 1
+
+type BatteryState struct {
+	Percentage int
+	Status     string
+	EnergyFull int
+	EnergyNow  int
+	PowerNow   int
+	Remaining  time.Duration
+	IsCharging bool
+	Mode       int
+}
+
+var batteryState = BatteryState{}
+
+func (b *BatteryState) Update() {
+	path := "/sys/class/power_supply/BAT0/"
+
+	percentage, err := ReadInt(path + "capacity")
+	if err != nil {
+		l.Println(err)
+	}
+	b.Percentage = percentage
+
+	status, err := ioutil.ReadFile(path + "status")
+	if err != nil {
+		l.Println(err)
+	}
+	b.Status = strings.Trim(string(status), "\n")
+	b.IsCharging = b.Status == "Charging"
+
+	power_now, err := ReadInt(path + "power_now")
+	if err != nil || power_now == 0 {
+		l.Println(err)
+		return
+	}
+	b.PowerNow = power_now
+
+	energy_now, err := ReadInt(path + "energy_now")
+	if err != nil {
+		l.Println(err)
+		return
+	}
+	b.EnergyNow = energy_now
+
+	energy_full, err := ReadInt(path + "energy_full")
+	if err != nil {
+		l.Println(err)
+		return
+	}
+	b.EnergyFull = energy_full
+
+	if b.IsCharging {
+		b.Remaining = time.Duration(((energy_full - energy_now) * 1000 / power_now)) * time.Hour / 1000
+	} else {
+		b.Remaining = time.Duration(b.EnergyNow*1000/b.PowerNow) * time.Hour / 1000
+	}
+}
 
 func StartClock(ch chan uint64, seconds int64, nanoseconds int64) {
 	fd, err := unix.TimerfdCreate(unix.CLOCK_REALTIME, 0)
@@ -55,89 +119,117 @@ func StartClock(ch chan uint64, seconds int64, nanoseconds int64) {
 	}
 }
 
-func PrintHeader() {
-	header_str, err := json.Marshal(Header{
-		Version: 1,
-	})
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	fmt.Println(string(header_str))
-	fmt.Println("[")
-}
-
-func MakeBlock(text string, urgent bool) string {
-	block := Body{
-		FullText: text,
-		Urgent:   urgent,
-	}
-	block_str, err := json.Marshal(block)
-
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	return string(block_str)
-}
-
 func GetCurrentTimeBlock(format string) string {
 	dt := time.Now()
-	return MakeBlock(dt.Format(format), false)
+	return MakeBlock("time", dt.Format(format), false)
 }
 
-func ReadInt64(file string) (int64, error) {
+func ReadInt(file string) (int, error) {
 	content, err := ioutil.ReadFile(file)
 	if err != nil {
 		return 0, err
 	}
-	value, _ := strconv.ParseInt(strings.Trim(string(content), "\n"), 10, 64)
-	return value, nil
+	value, _ := strconv.ParseInt(strings.Trim(string(content), "\n"), 10, 32)
+	return int(value), nil
 }
 
-func GetBatteryStatusBlock() string {
-	var symbol [5]string = [5]string{"\uf244", "\uf243", "\uf242", "\uf241", "\uf240"}
+func fmtDuration(d time.Duration) string {
+	d = d.Round(time.Minute)
+	h := d / time.Hour
+	d -= h * time.Hour
+	m := d / time.Minute
+	return fmt.Sprintf("%d:%02d", h, m)
+}
 
-	percentage, err := ReadInt64("/sys/class/power_supply/BAT0/capacity")
-	if err != nil {
-		return "??"
+func (b *BatteryState) GetBatteryStatusBlock() string {
+	var symbols [6]string = [6]string{"\uf244", "\uf243", "\uf242", "\uf241", "\uf240", "\uf240"}
+	var symbol = symbols[0]
+	var text = ""
+
+	if b.IsCharging {
+		symbol = "\uf1e6"
+	} else {
+		symbol = symbols[b.Percentage/20]
 	}
 
-	energy_now, err := ReadInt64("/sys/class/power_supply/BAT0/energy_now")
-	if err != nil {
-		return "??"
+	if b.Mode == BATTERY_STATE_MODE_PECENTAGE {
+		text = fmt.Sprintf("%s %d%%", symbol, b.Percentage)
+	} else if b.Mode == BATTERY_STATE_MODE_REMAINING_TIME {
+		text = fmt.Sprintf("%s %s", symbol, fmtDuration(b.Remaining))
 	}
 
-	power_now, err := ReadInt64("/sys/class/power_supply/BAT0/power_now")
-	if err != nil {
-		return "??"
+	return MakeBlock("power_supply", text, b.Percentage < 10)
+}
+
+func HandleClickEvents(ch chan ClickEvent, f *os.File) {
+	var event ClickEvent
+	scanner := bufio.NewScanner(os.Stdin)
+
+	jsonObjects := func(data []byte, atEOF bool) (advance int, token []byte, err error) {
+		inObject := 0
+		for i := 0; i < len(data); i++ {
+			switch data[i] {
+			case '{':
+				if inObject == 0 {
+					advance = i
+				}
+				inObject++
+			case '}':
+				inObject--
+				if inObject == 0 {
+					return i + 1, data[advance : i+1], nil
+				}
+			}
+		}
+		return
 	}
+	scanner.Split(jsonObjects)
 
-	hours := float64(energy_now) / float64(power_now)
-	hours_int := int64(hours)
-	minutes := int64((hours - float64(hours_int)) * 60)
+	for scanner.Scan() {
+		// skip first line
+		if scanner.Text() == "[" {
+			continue
+		}
+		err := json.Unmarshal(scanner.Bytes(), &event)
+		if err != nil {
+			fmt.Println(scanner.Text())
+			continue
+		}
+		ch <- event
+	}
+}
 
-	text := fmt.Sprintf("%s %d:%02d %d%%", symbol[percentage/20], hours_int, minutes, percentage)
-
-	return MakeBlock(text, percentage < 10)
+func Render() string {
+	return "[" +
+		batteryState.GetBatteryStatusBlock() + "," +
+		GetCurrentTimeBlock("15:04") +
+		"],"
 }
 
 func main() {
-	PrintHeader()
+	SendHeader()
+
+	f, _ := os.Create("/home/xtal/filename.ext")
+	defer f.Close()
+	defer os.Remove("/home/xtal/filename.ext")
 
 	ch := make(chan uint64)
+	events_ch := make(chan ClickEvent)
+
 	go StartClock(ch, 1, 0)
+	go HandleClickEvents(events_ch, f)
+
+	// batteryState.Mode = BATTERY_STATE_MODE_REMAINING_TIME
 
 	for {
 		select {
 		case <-ch:
-			fmt.Println("[" +
-				GetBatteryStatusBlock() + "," +
-				GetCurrentTimeBlock("15:04") +
-				"],")
+			batteryState.Update()
+		case event := <-events_ch:
+			if event.Name == "power_supply" {
+				batteryState.Mode = (batteryState.Mode + 1) % 2
+			}
 		}
+		fmt.Println(Render())
 	}
 }
--- swaybar.go
+++ swaybar.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+)
+
 type Header struct {
 	Version     int  `json:"version"`
 	ClickEvents bool `json:"click_events,omitempty"`
@@ -26,4 +32,43 @@ type Body struct {
 	Separator           bool   `json:"separator,omitempty"`
 	SeparatorBlockWidth int    `json:"separator_block_width,omitempty"`
 	Markup              string `json:"markup,omitempty"`
+}
+
+type ClickEvent struct {
+	Name      string `json:"name"`
+	Instance  string `json:"instance"`
+	X         int    `json:"x"`
+	Y         int    `json:"y"`
+	Button    int    `json:"button"`
+	Event     int    `json:"event"`
+	RelativeX int    `json:"relative_x"`
+	RelativeY int    `json:"relative_y"`
+	Width     int    `json:"width"`
+	Height    int    `json:"height"`
+}
+
+func SendHeader() {
+	header_str, _ := json.Marshal(Header{
+		Version:     1,
+		ClickEvents: true,
+	})
+
+	fmt.Println(string(header_str))
+	fmt.Println("[")
+}
+
+func MakeBlock(name string, full_text string, urgent bool) string {
+	block := Body{
+		FullText: full_text,
+		Name:     name,
+		Urgent:   urgent,
+	}
+	block_str, err := json.Marshal(block)
+
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	return string(block_str)
 }
